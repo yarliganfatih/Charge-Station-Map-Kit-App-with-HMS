ace305698bc74be8370a03f63464d5d44086d6d4
Fatih Yarlıgan <fatih.yarligan@bahcesehir.edu.tr>
2023-01-12 02:40:24 +0300

    Search Fragment designed

--- app/src/main/java/com/example/hmsmapkitapp/SearchFragment.kt
+++ app/src/main/java/com/example/hmsmapkitapp/SearchFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.hmsmapkitapp.databinding.FragmentFirstBinding
+import com.example.hmsmapkitapp.databinding.FragmentSearchBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the Search destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,20 +24,13 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-        binding.buttonSearch.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SearchFragment)
-        }
     }
 
     override fun onDestroyView() {
